810801d08db08c369ec8a594463b05ac314bad19
Cristian Prieto <me@cprieto.com>
2014-02-23 21:04:04 +1100

    Now we have a Cat class

---
+++ Cat.m
@@ -0,0 +1,5 @@
+#include "Cat.h"
+
+@implementation Cat
+
+@end
--- main.m
+++ main.m
@@ -1,7 +1,9 @@
 #include <Foundation/Foundation.h>
+#include "Cat.h"
 
 int main(int argc, const char* argv[])
 {
+	Cat *cat = [[Cat alloc] init];
 	NSLog(@"Hello world");
 	return 0;
 }
